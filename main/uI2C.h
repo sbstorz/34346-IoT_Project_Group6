e38f94d8dedcc23d8c764efba86e2c69bcf03608
mraabo <magnusraabo@gmail.com>
2025-04-04 11:22:29 +0200

    add I2C functionality

---
+++ main/uI2C.h
@@ -0,0 +1,13 @@
+#ifndef UI2C_H_
+#define UI2C_H_
+
+
+#include "driver/i2c_master.h"
+
+#define SCL_IO_PIN 36
+#define SDA_IO_PIN 33
+
+void i2c_init_master(void);
+esp_err_t i2c_add_slave(uint16_t address, i2c_master_dev_handle_t *dev_handle);
+
+#endif // UI2C_H_
