53b1da9fa3003852dcc47136ef24bd456eeb1d5d
Stepzi <s.storz01@gmail.com>
2025-04-08 14:13:42 +0200

    RN wake and sleep

--- main/main.c
+++ main/main.c
@@ -19,14 +19,16 @@ void app_main(void)
 
     while (1)
     {
-        if (rn_tx("Sunny Day!", 1, true, rx_data, 10, &port) == ESP_OK && port > 0)
-        {
-            ESP_LOGI(TAG, "Received RX, port: %d, data:", port);
-            ESP_LOG_BUFFER_HEXDUMP(TAG, rx_data, strlen(rx_data), ESP_LOG_INFO);
-        }
+        // if (rn_tx("Sunny Day!", 1, true, rx_data, 10, &port) == ESP_OK && port > 0)
+        // {
+        //     ESP_LOGI(TAG, "Received RX, port: %d, data:", port);
+        //     ESP_LOG_BUFFER_HEXDUMP(TAG, rx_data, strlen(rx_data), ESP_LOG_INFO);
+        // }
         ESP_LOGI(TAG, "VDD: %s", rn_send_raw_cmd("sys get vdd"));
         ESP_LOGI(TAG, "VER: %s", rn_send_raw_cmd("sys get ver"));
-        sleep(60);
+        ESP_ERROR_CHECK_WITHOUT_ABORT(rn_sleep());
+        sleep(5);
+        ESP_ERROR_CHECK_WITHOUT_ABORT(rn_wake());
     }
 
 }
